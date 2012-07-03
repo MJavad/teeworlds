d7c6ced26a8a9951ef0f35f6725e2628ce8b765f
MAP94 <marcopatz@gmx.de>
2012-07-03 16:49:14 +0200

    Added missing files

---
+++ src/game/luaevent.cpp
@@ -0,0 +1,162 @@
+#include "luaevent.h"
+#include <base/system.h>
+//storage class for the event variables
+
+CEventVariable::CEventVariable()
+{
+    m_Type = EVENT_TYPE_INVALID;
+    m_pData = 0;
+    m_Size = 0;
+}
+
+CEventVariable::~CEventVariable()
+{
+    if (m_pData)
+        delete []m_pData;
+}
+
+void CEventVariable::Allocate(int Size)
+{
+    if (m_pData)
+        delete []m_pData;
+    m_Size = Size;
+    m_pData = 0;
+    if (Size > 0)
+        m_pData = new char[Size];
+}
+
+//allocate 16 bytes even if we need 4 or just 1
+//why?
+//because when we transform this var to something bigger
+//bool -> int
+void CEventVariable::Set(int Value)
+{
+    m_Type = EVENT_TYPE_INTEGER;
+    Allocate(16);
+    mem_copy(m_pData, &Value, sizeof(Value));
+}
+
+void CEventVariable::Set(float Value)
+{
+    m_Type = EVENT_TYPE_FLOAT;
+    Allocate(16);
+    mem_copy(m_pData, &Value, sizeof(Value));
+}
+
+void CEventVariable::Set(bool Value)
+{
+    m_Type = EVENT_TYPE_BOOL;
+    Allocate(16);
+    mem_copy(m_pData, &Value, sizeof(Value));
+}
+
+void CEventVariable::Set(char Value)
+{
+    m_Type = EVENT_TYPE_CHAR;
+    Allocate(16);
+    mem_copy(m_pData, &Value, sizeof(Value));
+}
+
+void CEventVariable::Set(const char *pStr)
+{
+    m_Type = EVENT_TYPE_STRING;
+    Allocate(str_length(pStr) + 1);
+    mem_copy(m_pData, pStr, str_length(pStr) + 1);
+}
+
+void CEventVariable::Set(const char *pData, int Size)
+{
+    m_Type = EVENT_TYPE_STRING;
+    Allocate(Size);
+    mem_copy(m_pData, pData, Size);
+}
+
+void *CEventVariable::Get()
+{
+    return m_pData;
+}
+
+int CEventVariable::GetSize()
+{
+    return m_Size;
+}
+
+char *CEventVariable::GetString()
+{
+    return (char *)m_pData;
+}
+
+int CEventVariable::GetInteger()
+{
+    if (IsNumeric() == false)
+        return 0;
+    if (!m_pData)
+        return 0;
+    return *((int *)m_pData);
+}
+
+float CEventVariable::GetFloat()
+{
+    if (IsNumeric() == false)
+        return 0;
+    if (!m_pData)
+        return 0;
+    return *((float *)m_pData);
+}
+
+bool CEventVariable::GetBool()
+{
+    if (IsNumeric() == false)
+        return 0;
+    if (!m_pData)
+        return 0;
+    return *((bool *)m_pData);
+}
+
+char CEventVariable::GetChar()
+{
+    if (IsNumeric() == false)
+        return 0;
+    if (!m_pData)
+        return 0;
+    return *((char *)m_pData);
+}
+
+int CEventVariable::GetType()
+{
+    return m_Type;
+}
+
+bool CEventVariable::IsNumeric()
+{
+    return !(!(m_Type & (EVENT_TYPE_INTEGER | EVENT_TYPE_FLOAT | EVENT_TYPE_BOOL | EVENT_TYPE_CHAR)));
+}
+
+void CEventVariable::Reset()
+{
+    Allocate(0); //free
+    m_Type = EVENT_TYPE_INVALID;
+}
+
+void CEvent::Reset()
+{
+    for (int i = 0; i < MAX_EVENT_VARIABLES; i++)
+    {
+        m_aVars[i].Reset();
+    }
+}
+
+CEventVariable *CEvent::FindFree()
+{
+    for (int i = 0; i < MAX_EVENT_VARIABLES; i++)
+    {
+        if (m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_INVALID)
+        {
+            return &m_aVars[i];
+        }
+    }
+    return 0;
+}
+
+
+//Eventlistener -> .h
---
+++ src/game/luaevent.h
@@ -0,0 +1,220 @@
+#ifndef GAME_LUAEVENT_H
+#define GAME_LUAEVENT_H
+
+#include <base/tl/array.h>
+extern "C" { // lua
+    #define LUA_CORE /* make sure that we don't try to import these functions */
+    #include <engine/external/lua/lua.h>
+    #include <engine/external/lua/lualib.h> /* luaL_openlibs */
+    #include <engine/external/lua/lauxlib.h> /* luaL_loadfile */
+}
+#ifdef GAME_CLIENT_LUA_H
+#include "client/lua.h"
+#endif
+#ifdef GAME_SERVER_LUA_H
+#include "server/lua/lua.h"
+#endif
+
+//storage class for the event variables
+
+class CEventVariable
+{
+    int m_Type;
+    void *m_pData;
+    int m_Size;
+    void Allocate(int Size);
+public:
+    CEventVariable();
+    ~CEventVariable();
+    void Set(int Value);
+    void Set(float Value);
+    void Set(bool Value);
+    void Set(char Value);
+    void Set(const char *pStr);
+    void Set(const char *pData, int Size);
+    void *Get();
+    int GetSize();
+    int GetInteger();
+    float GetFloat();
+    bool GetBool();
+    char GetChar();
+    char *GetString();
+    int GetType();
+    bool IsNumeric();
+    void Reset();
+
+    enum
+    {
+        EVENT_TYPE_INVALID = 0,
+        EVENT_TYPE_INTEGER = 1,
+        EVENT_TYPE_FLOAT = 2,
+        EVENT_TYPE_BOOL = 4,
+        EVENT_TYPE_CHAR = 8,
+        EVENT_TYPE_STRING = 16,
+        EVENT_TYPE_DATA = 32
+    };
+};
+
+
+#define MAX_EVENT_VARIABLES 64
+class CEvent
+{
+public:
+    CEventVariable m_aVars[MAX_EVENT_VARIABLES];
+    void Reset();
+    CEventVariable *FindFree();
+};
+
+template <class T>
+class CLuaEventListener
+{
+    struct CLuaListenerData
+    {
+        T *m_pLuaFile;
+        char m_aLuaFunction[256];
+        char m_aEvent[256];
+        bool operator==(const CLuaListenerData &Other) { return this == &Other; }
+    };
+
+    array<CLuaListenerData> m_aListeners;
+public:
+    void AddEventListener(T *pLuaFile, char *pEvent, char *pLuaFunction);
+    void RemoveEventListener(T *pLuaFile, char *pEvent);
+    void RemoveAllEventListeners(T *pLuaFile);
+
+    void OnEvent(const char *pEvent);
+
+    CEvent m_Parameters;
+    CEvent m_Returns;
+
+    CLuaEventListener();
+    ~CLuaEventListener();
+};
+
+//Eventlistener
+template <class T>
+CLuaEventListener<T>::CLuaEventListener()
+{
+    m_aListeners.clear();
+}
+template <class T>
+CLuaEventListener<T>::~CLuaEventListener()
+{
+    m_aListeners.clear();
+}
+
+template <class T>
+void CLuaEventListener<T>::AddEventListener(T *pLuaFile, char *pEvent, char *pLuaFunction)
+{
+    CLuaListenerData Listener;
+    Listener.m_pLuaFile = pLuaFile;
+    str_copy(Listener.m_aLuaFunction, pLuaFunction, sizeof(Listener.m_aLuaFunction));
+    str_copy(Listener.m_aEvent, pEvent, sizeof(Listener.m_aEvent));
+    m_aListeners.add(Listener);
+}
+
+template <class T>
+void CLuaEventListener<T>::OnEvent(const char *pEvent)
+{
+    m_Returns.Reset();
+	for(array<CLuaListenerData>::range r = m_aListeners.all(); !r.empty(); r.pop_front())
+    {
+        if (r.front().m_aEvent && str_comp_nocase(r.front().m_aEvent, pEvent) == 0)
+        {
+            if (r.front().m_pLuaFile->FunctionExist(r.front().m_aLuaFunction))
+            {
+                int Num = lua_gettop(r.front().m_pLuaFile->m_pLua); //get stack size before calling fx
+                int Start = lua_gettop(r.front().m_pLuaFile->m_pLua); //get stack size before calling fx
+                r.front().m_pLuaFile->FunctionPrepare(r.front().m_aLuaFunction);
+                for (int i = 0; i < MAX_EVENT_VARIABLES; i++)
+                {
+                    if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_INTEGER)
+                    {
+                        r.front().m_pLuaFile->PushInteger(m_Parameters.m_aVars[i].GetInteger());
+                    }
+                    else if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_CHAR)
+                    {
+                        r.front().m_pLuaFile->PushInteger(m_Parameters.m_aVars[i].GetInteger());
+                    }
+                    else if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_FLOAT)
+                    {
+                        r.front().m_pLuaFile->PushFloat(m_Parameters.m_aVars[i].GetInteger());
+                    }
+                    else if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_BOOL)
+                    {
+                        r.front().m_pLuaFile->PushBoolean(m_Parameters.m_aVars[i].GetInteger());
+                    }
+                    else if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_STRING)
+                    {
+                        r.front().m_pLuaFile->PushString(m_Parameters.m_aVars[i].GetString());
+                    }
+                    else if (m_Parameters.m_aVars[i].GetType() == CEventVariable::EVENT_TYPE_DATA)
+                    {
+                        r.front().m_pLuaFile->PushData((char *)m_Parameters.m_aVars[i].Get(), m_Parameters.m_aVars[i].GetSize());
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                r.front().m_pLuaFile->FunctionExec();
+                Num = lua_gettop(r.front().m_pLuaFile->m_pLua) - Num; //get number of returns
+                for (int i = 0; i < Num; i++) //lua stack begins with 1 - so manipulate every i
+                {
+                    if (lua_isboolean(r.front().m_pLuaFile->m_pLua, Start + i + 1))
+                    {
+                        m_Returns.m_aVars[i].Set(lua_toboolean(r.front().m_pLuaFile->m_pLua, Start + i + 1));
+                    }
+                    if (lua_isnumber(r.front().m_pLuaFile->m_pLua, Start + i + 1))
+                    {
+                        if (lua_tointeger(r.front().m_pLuaFile->m_pLua, Start + i + 1) == lua_tonumber(r.front().m_pLuaFile->m_pLua, Start + i + 1))
+                            m_Returns.m_aVars[i].Set(lua_tointeger(r.front().m_pLuaFile->m_pLua, Start + i + 1));
+                        else
+                            m_Returns.m_aVars[i].Set((float)lua_tonumber(r.front().m_pLuaFile->m_pLua, Start + i + 1));
+                    }
+                    if (lua_isstring(r.front().m_pLuaFile->m_pLua, Start + i + 1))
+                    {
+                        int Size = 0;
+                        const char *pData = lua_tolstring(r.front().m_pLuaFile->m_pLua, Start + i + 1, (size_t *)&Size);
+                        if (str_length(pData) == Size)
+                            m_Returns.m_aVars[i].Set(pData);
+                        else
+                            m_Returns.m_aVars[i].Set(pData, Size);
+                    }
+                }
+                for (int i = 0; i < Num; i++) //pop values
+                {
+                    lua_remove(r.front().m_pLuaFile->m_pLua, -1);
+                    //lua_pop(r.front().m_pLuaFile->m_pLua, 1);
+                }
+            }
+        }
+    }
+    m_Parameters.Reset();
+}
+
+template <class T>
+void CLuaEventListener<T>::RemoveEventListener(T *pLuaFile, char *pEvent)
+{
+	for(array<CLuaListenerData>::range r = m_aListeners.all(); !r.empty(); r.pop_front())
+    {
+        if (r.front().m_pLuaFile == pLuaFile && str_comp_nocase(r.front().m_aEvent, pEvent) == 0)
+        {
+            m_aListeners.remove(r.front());
+        }
+    }
+}
+
+template <class T>
+void CLuaEventListener<T>::RemoveAllEventListeners(T *pLuaFile)
+{
+	for(array<CLuaListenerData>::range r = m_aListeners.all(); !r.empty(); r.pop_front())
+    {
+        if (r.front().m_pLuaFile == pLuaFile)
+        {
+            m_aListeners.remove(r.front());
+        }
+    }
+}
+
+#endif
